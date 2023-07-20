fda858bf9e605a7fefa259418f524e1ee94f735f
RageNeko26 <rneko2006@gmail.com>
2023-07-20 10:58:46 +0700

    feat: Add test case

--- symmetric/decrypt_test.go
+++ symmetric/decrypt_test.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"testing"
+
+	"github.com/o1egl/paseto"
 )
 
 func TestDecrypt(t *testing.T) {
-	// errs := paseto.NewV1().Decrypt()
+	var newJSONToken paseto.JSONToken
+	var newFooter string
+
+	token := Encrypt()
+
+	err := paseto.NewV1().Decrypt(token, symmetricKey, &newJSONToken, &newFooter)
+
+	/*
+	* Case: Error should be nill
+	* Or there should no error
+	 */
+	if err != nil {
+		t.Error("There is error", err)
+	}
+
 }
